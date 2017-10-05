feca1dfeee778deff44cd85dea91035e0d096ac5
Sunjay E. Dixit <jaydixit.work@gmail.com>
2017-10-05 00:25:44 -0400

    dark and stormy

---
+++ darkandstormy/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/dudleystorey/pen/PZyMrd.
+
+ [Full explanatory article on my site](http://thenewcode.com/500/Use-YouTube-Videos-as-Fullscreen-Web-Page-Backgrounds).
---
+++ youtube-vertical-playlist-responsive/js/index.js
@@ -0,0 +1,10 @@
+/*JS FOR SCROLLING THE ROW OF THUMBNAILS*/ 
+$(document).ready(function () {
+  $('.vid-item').each(function(index){
+    $(this).on('click', function(){
+      var current_index = index+1;
+      $('.vid-item .thumb').removeClass('active');
+      $('.vid-item:nth-child('+current_index+') .thumb').addClass('active');
+    });
+  });
+});
